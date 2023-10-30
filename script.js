68e78912284cbe7b34b395ebf47d983b16a2e633
Kevin-Moreau65 <kevin.moreau655@gmail.com>
2023-10-30 10:33:32 +0100

    Upgrade

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 // Eléments du DOM fréquemment utilisés
-const heureArriveeElem = document.getElementById("heureArrivee");
-const heurePauseElem = document.getElementById("heurePause");
-const heureRepriseElem = document.getElementById("heureReprise");
-const dureeTravailElem = document.getElementById("dureeTravail"); // Ce sera maintenant une chaîne au format "HH:mm"
-const resultatElem = document.getElementById("resultat");
-const historiqueElem = document.getElementById("historique");
+const heureArriveeElem = document.getElementById( "heureArrivee" );
+const heurePauseElem = document.getElementById( "heurePause" );
+const heureRepriseElem = document.getElementById( "heureReprise" );
+const dureeTravailElem = document.getElementById( "dureeTravail" ); // Ce sera maintenant une chaîne au format "HH:mm"
+const resultatElem = document.getElementById( "resultat" );
+const historiqueElem = document.getElementById( "historique" );
+const themeButton = document.getElementById( "themeToggleButton" );
+const versionElem = document.getElementById( "appVersion" )
 
-const appVersion = "1.2"; // Remplacez par la version actuelle de l'application
+const appVersion = "1.3"; // Remplacez par la version actuelle de l'application
 
-const versionElem = document.createElement("div");
-versionElem.id = "appVersion";
 versionElem.textContent = "Version " + appVersion + " - " + "Oct 2023";
-document.body.appendChild(versionElem);
 
 // Fonction pour formater la durée en un format lisible
-const formatDuration = (duration) => {
-  const [hours, minutes] = duration.split(":");
-  return `${parseInt(hours, 10)} heure(s) ${
-    parseInt(minutes, 10) > 0 ? parseInt(minutes, 10) + " minute(s)" : ""
-  }`;
+const formatDuration = ( duration ) => {
+  const [ hours, minutes ] = duration.split( ":" );
+  return `${ parseInt( hours, 10 ) } heure(s) ${ parseInt( minutes, 10 ) > 0 ? parseInt( minutes, 10 ) + " minute(s)" : ""
+    }`;
 };
 
 // Fonction utilitaire pour convertir une heure en objet Date
-const convertToDateTime = (heureStr) => {
+const convertToDateTime = ( heureStr ) => {
   const currentDate = new Date();
-  const [hour, minute] = heureStr.split(":");
+  const [ hour, minute ] = heureStr.split( ":" );
   return new Date(
     currentDate.getFullYear(),
     currentDate.getMonth(),
@@ -35,41 +33,38 @@ const convertToDateTime = (heureStr) => {
 };
 
 // Vérifiez le mode stocké lors du chargement de la page
-const storedTheme = localStorage.getItem("theme-mode");
-if (storedTheme) {
-  document.documentElement.setAttribute("data-theme", storedTheme);
+const storedTheme = localStorage.getItem( "theme-mode" );
+if ( storedTheme ) {
+  document.documentElement.setAttribute( "data-theme", storedTheme );
 }
 
 // Ajout du bouton de basculement du mode sombre
-const themeButton = document.createElement("button");
-themeButton.id = "themeToggleButton";
-themeButton.textContent = storedTheme === "dark" ? "Mode clair" : "Mode sombre";
-document.body.appendChild(themeButton);
+themeButton.innerHTML = storedTheme === "dark" ? '<img src="img/mode-clair.png" alt="">' : '<img src="img/mode-sombre.png" alt="">';
 
-themeButton.addEventListener("click", () => {
-  if (document.documentElement.getAttribute("data-theme") === "dark") {
-    document.documentElement.removeAttribute("data-theme");
-    themeButton.textContent = "Mode sombre";
-    localStorage.removeItem("theme-mode");
+themeButton.addEventListener( "click", () => {
+  if ( document.documentElement.getAttribute( "data-theme" ) === "dark" ) {
+    document.documentElement.removeAttribute( "data-theme" );
+    themeButton.innerHTML = '<img src="img/mode-sombre.png" alt="">';
+    localStorage.removeItem( "theme-mode" );
   } else {
-    document.documentElement.setAttribute("data-theme", "dark");
-    themeButton.textContent = "Mode clair";
-    localStorage.setItem("theme-mode", "dark");
+    document.documentElement.setAttribute( "data-theme", "dark" );
+    themeButton.innerHTML = '<img src="img/mode-clair.png" alt="">';
+    localStorage.setItem( "theme-mode", "dark" );
   }
-});
+} );
 
 // Fonction pour ajouter une entrée à l'historique avec un bouton de suppression
-function ajouterEntreeHistorique(texte) {
-  const li = document.createElement("li");
-  const container = document.createElement("div"); // Conteneur pour aligner le bouton et le texte
+function ajouterEntreeHistorique ( texte ) {
+  const li = document.createElement( "li" );
+  const container = document.createElement( "div" ); // Conteneur pour aligner le bouton et le texte
   container.className = "list-item-container"; // Utiliser la classe CSS pour le style
   // Création du bouton de suppression
-  const supprimerBtn = document.createElement("button");
+  const supprimerBtn = document.createElement( "button" );
   supprimerBtn.textContent = "X"; // X au lieu du texte "Supprimer"
   supprimerBtn.className = "delete-btn"; // Appliquer la classe CSS
   supprimerBtn.onclick = function () {
     // Confirmation avant suppression
-    Swal.fire({
+    Swal.fire( {
       title: "Êtes-vous sûr?",
       text: "Vous ne pourrez pas revenir en arrière !",
       icon: "warning",
@@ -78,19 +73,19 @@ function ajouterEntreeHistorique(texte) {
       cancelButtonColor: "#d33",
       confirmButtonText: "Oui",
       cancelButtonText: "Non",
-    }).then((result) => {
-      if (result.isConfirmed) {
+    } ).then( ( result ) => {
+      if ( result.isConfirmed ) {
         // Suppression de l'entrée de l'UI
         li.remove();
 
         // Suppression de l'entrée du localStorage
         const historiqueArr =
-          JSON.parse(localStorage.getItem("historiqueArr")) || [];
-        const index = historiqueArr.indexOf(texte);
-        if (index > -1) {
-          historiqueArr.splice(index, 1);
+          JSON.parse( localStorage.getItem( "historiqueArr" ) ) || [];
+        const index = historiqueArr.indexOf( texte );
+        if ( index > -1 ) {
+          historiqueArr.splice( index, 1 );
         }
-        localStorage.setItem("historiqueArr", JSON.stringify(historiqueArr));
+        localStorage.setItem( "historiqueArr", JSON.stringify( historiqueArr ) );
 
         // Affichage de l'alerte de confirmation de suppression
         Swal.fire(
@@ -99,20 +94,20 @@ function ajouterEntreeHistorique(texte) {
           "success"
         );
       }
-    });
+    } );
   };
 
   // Ajout du bouton et du texte au conteneur
-  container.appendChild(supprimerBtn); // Le bouton d'abord pour qu'il apparaisse à gauche
-  container.appendChild(document.createTextNode(texte)); // Utiliser createTextNode pour éviter des problèmes d'espacement
+  container.appendChild( supprimerBtn ); // Le bouton d'abord pour qu'il apparaisse à gauche
+  container.appendChild( document.createTextNode( texte ) ); // Utiliser createTextNode pour éviter des problèmes d'espacement
 
   // Ajout du conteneur à l'élément de la liste
-  li.appendChild(container);
-  historiqueElem.appendChild(li);
+  li.appendChild( container );
+  historiqueElem.appendChild( li );
 }
 
 // Gestionnaire d'événements pour le bouton de calcul
-document.getElementById("calculButton").addEventListener("click", () => {
+document.getElementById( "calculButton" ).addEventListener( "click", () => {
   // Récupération des valeurs des champs du formulaire
   const heureArrivee = heureArriveeElem.value;
   const heurePause = heurePauseElem.value;
@@ -120,8 +115,8 @@ document.getElementById("calculButton").addEventListener("click", () => {
   const dureeTravail = dureeTravailElem.value; // Ce sera maintenant une chaîne au format "HH:mm"
 
   // Conversion de la durée de travail en minutes totales
-  const [hours, minutes] = dureeTravail.split(":");
-  const dureeTravailMinutes = parseInt(hours, 10) * 60 + parseInt(minutes, 10);
+  const [ hours, minutes ] = dureeTravail.split( ":" );
+  const dureeTravailMinutes = parseInt( hours, 10 ) * 60 + parseInt( minutes, 10 );
 
   // Vérification de la validité des entrées
   if (
@@ -131,35 +126,35 @@ document.getElementById("calculButton").addEventListener("click", () => {
     dureeTravailMinutes <= 0 ||
     dureeTravailMinutes > 12 * 60
   ) {
-    Swal.fire({
+    Swal.fire( {
       icon: "error",
       title: "Erreur",
       text: "Veuillez remplir tous les champs correctement. Assurez-vous que la durée de travail est comprise entre 1 minute et 12 heures.",
-    });
+    } );
     return;
   }
 
   // Conversion des heures entrées en objets Date pour faciliter les opérations
-  const heureArriveeObj = convertToDateTime(heureArrivee);
-  const heurePauseObj = convertToDateTime(heurePause);
-  const heureRepriseObj = convertToDateTime(heureReprise);
+  const heureArriveeObj = convertToDateTime( heureArrivee );
+  const heurePauseObj = convertToDateTime( heurePause );
+  const heureRepriseObj = convertToDateTime( heureReprise );
 
   // Vérification des horaires pour s'assurer de leur cohérence
-  if (heureArriveeObj >= heurePauseObj || heurePauseObj >= heureRepriseObj) {
-    Swal.fire({
+  if ( heureArriveeObj >= heurePauseObj || heurePauseObj >= heureRepriseObj ) {
+    Swal.fire( {
       icon: "error",
       title: "Erreur",
       text: "Vérifiez vos horaires ! L'heure d'arrivée doit être antérieure à l'heure de pause et l'heure de pause doit être antérieure à l'heure de reprise.",
-    });
+    } );
     return;
   }
 
   // Si l'heure de reprise est avant l'heure de pause, cela signifie que la pause s'étend au-delà de minuit
-  if (heureRepriseObj < heurePauseObj)
-    heureRepriseObj.setDate(heureRepriseObj.getDate() + 1);
+  if ( heureRepriseObj < heurePauseObj )
+    heureRepriseObj.setDate( heureRepriseObj.getDate() + 1 );
 
   // Calcul de la durée de la pause en minutes
-  const pauseDuration = (heureRepriseObj - heurePauseObj) / (1000 * 60);
+  const pauseDuration = ( heureRepriseObj - heurePauseObj ) / ( 1000 * 60 );
 
   // Vérifications supplémentaires sur la validité des horaires
   if (
@@ -167,16 +162,16 @@ document.getElementById("calculButton").addEventListener("click", () => {
     heureArriveeObj >= heurePauseObj ||
     heurePauseObj >= heureRepriseObj
   ) {
-    Swal.fire({
+    Swal.fire( {
       icon: "error",
       title: "Erreur",
       text: "Vérifiez vos horaires ! Assurez-vous que la durée de pause est d'au moins 30 minutes et que les horaires sont cohérents.",
-    });
+    } );
     return;
   }
 
   // Calcul du temps travaillé avant la pause
-  const totalMinutes = (heurePauseObj - heureArriveeObj) / (1000 * 60);
+  const totalMinutes = ( heurePauseObj - heureArriveeObj ) / ( 1000 * 60 );
   const heuresTravailRestant = dureeTravailMinutes - totalMinutes;
 
   // Calcul de l'heure de fin de travail
@@ -185,57 +180,57 @@ document.getElementById("calculButton").addEventListener("click", () => {
   );
 
   // Formatage de l'heure de fin pour l'affichage
-  const heureFinFormat = FinCalcul.toLocaleTimeString([], {
+  const heureFinFormat = FinCalcul.toLocaleTimeString( [], {
     hour: "2-digit",
     minute: "2-digit",
-  });
+  } );
 
   // Mise à jour de l'élément de résultat avec un effet d'opacité pour attirer l'attention
   resultatElem.style.opacity = 0;
-  setTimeout(() => {
+  setTimeout( () => {
     resultatElem.style.opacity = 1;
-    resultatElem.innerHTML = `Vous devez finir à ${heureFinFormat} pour travailler ${formatDuration(
+    resultatElem.innerHTML = `Vous devez finir à ${ heureFinFormat } pour travailler ${ formatDuration(
       dureeTravail
-    )} au total.`; // Utilisation de la fonction de formatage ici
-  }, 50);
+    ) } au total.`; // Utilisation de la fonction de formatage ici
+  }, 50 );
 
   // Ajout du résultat à la liste de l'historique
   const formattedDate = new Date().toLocaleDateString();
-  const texteHistorique = `${formattedDate} - Durée de travail : ${formatDuration(
+  const texteHistorique = `${ formattedDate } - Durée de travail : ${ formatDuration(
     dureeTravail
-  )}, Arrivée à ${heureArrivee}, Pause à ${heurePause}, Reprise à ${heureReprise} => Fin de la journée à ${heureFinFormat}`; // Utilisation de la fonction de formatage ici
+  ) }, Arrivée à ${ heureArrivee }, Pause à ${ heurePause }, Reprise à ${ heureReprise } => Fin de la journée à ${ heureFinFormat }`; // Utilisation de la fonction de formatage ici
 
   // Utilisation de la fonction pour ajouter une entrée à l'historique
-  ajouterEntreeHistorique(texteHistorique);
+  ajouterEntreeHistorique( texteHistorique );
 
   // Enregistrement du résultat dans le localStorage pour une persistance entre les sessions
-  const historiqueArr = JSON.parse(localStorage.getItem("historiqueArr")) || [];
-  historiqueArr.push(texteHistorique);
-  localStorage.setItem("historiqueArr", JSON.stringify(historiqueArr));
-});
+  const historiqueArr = JSON.parse( localStorage.getItem( "historiqueArr" ) ) || [];
+  historiqueArr.push( texteHistorique );
+  localStorage.setItem( "historiqueArr", JSON.stringify( historiqueArr ) );
+} );
 
 // Au chargement du document, récupération et affichage de l'historique depuis le localStorage
-document.addEventListener("DOMContentLoaded", () => {
-  const historiqueArr = JSON.parse(localStorage.getItem("historiqueArr")) || [];
-  historiqueArr.forEach(ajouterEntreeHistorique); // Utilisation de la nouvelle fonction ici
-});
+document.addEventListener( "DOMContentLoaded", () => {
+  const historiqueArr = JSON.parse( localStorage.getItem( "historiqueArr" ) ) || [];
+  historiqueArr.forEach( ajouterEntreeHistorique ); // Utilisation de la nouvelle fonction ici
+} );
 
 // Gestionnaire d'événements pour le bouton d'effacement de l'historique
-document.getElementById("clearHistoryButton").addEventListener("click", () => {
-  const historiqueArr = JSON.parse(localStorage.getItem("historiqueArr")) || [];
+document.getElementById( "clearHistoryButton" ).addEventListener( "click", () => {
+  const historiqueArr = JSON.parse( localStorage.getItem( "historiqueArr" ) ) || [];
 
   // Si l'historique est vide
-  if (historiqueArr.length === 0) {
-    Swal.fire({
+  if ( historiqueArr.length === 0 ) {
+    Swal.fire( {
       icon: "error",
       title: "Erreur",
       text: "Il n'y a pas d'historique à supprimer.",
-    });
+    } );
     return;
   }
 
   // Confirmation avant suppression
-  Swal.fire({
+  Swal.fire( {
     title: "Êtes-vous sûr?",
     text: "Vous ne pourrez pas revenir en arrière !",
     icon: "warning",
@@ -244,18 +239,18 @@ document.getElementById("clearHistoryButton").addEventListener("click", () => {
     cancelButtonColor: "#d33",
     confirmButtonText: "Oui",
     cancelButtonText: "Non",
-  }).then((result) => {
-    if (result.isConfirmed) {
+  } ).then( ( result ) => {
+    if ( result.isConfirmed ) {
       // Suppression de l'historique du localStorage
-      localStorage.removeItem("historiqueArr");
+      localStorage.removeItem( "historiqueArr" );
 
       // Effacement de la liste d'historique à l'écran
-      while (historiqueElem.firstChild) {
-        historiqueElem.removeChild(historiqueElem.firstChild);
+      while ( historiqueElem.firstChild ) {
+        historiqueElem.removeChild( historiqueElem.firstChild );
       }
 
       // Notification à l'utilisateur que l'historique a été effacé
-      Swal.fire("Supprimé!", "Votre historique a été supprimé.", "success");
+      Swal.fire( "Supprimé!", "Votre historique a été supprimé.", "success" );
     }
-  });
-});
+  } );
+} );
